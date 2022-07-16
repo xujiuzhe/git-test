950fc4249b5f7585a838516e226f71d331ab8c7e
xjz <xjz@qut.com>
2022-07-16 15:34:33 +0800

    third commit

--- src/main/java/com/xjz/git/GitTest.java
+++ src/main/java/com/xjz/git/GitTest.java
@@ -3,12 +3,8 @@ package com.xjz.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git!");
         System.out.println("hello git2!");
-
-
+        System.out.println("hello git3!");
 
     }
 }
