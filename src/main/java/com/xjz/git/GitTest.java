de11ff669ef4c0f3e85c079ada6a916077e711cb
xjz <xjz@qut.com>
2022-07-16 15:21:36 +0800

    first commit

---
+++ src/main/java/com/xjz/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.xjz.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+
+
+
+    }
+}
